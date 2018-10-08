ef5ddda8e77487c3301516dbf9333503203077c5
MasayoshiNomura <masayoshi.nomura@outlook.jp>
2018-10-08 17:44:53 +0900

    Initial Commit

---
+++ MyFirsrApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirsrApp
+//
+//  Created by Masayoshi Nomura on 2018/10/07.
+//  Copyright © 2018年 Masayoshi Nomura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
