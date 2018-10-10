c58610e714c369fde70f331fe9fe9655bce8c938
MasayoshiNomura <masayoshi.nomura@outlook.jp>
2018-10-10 14:35:57 +0900

    I'm not sure how to use GitHub. lol Please, anyone help me!!

--- MyFirsrApp/ViewController.swift
+++ MyFirsrApp/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var label: UITextField!
     
+    @IBOutlet weak var cameraView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +21,9 @@ class ViewController: UIViewController {
 
     @IBAction func UIbutton(_ sender: Any) {
         label.text = "correct!"
+    }
+    
+    @IBAction func camera(_ sender: UIButton) {
     }
     
 }
