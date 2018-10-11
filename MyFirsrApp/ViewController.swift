cdc988d3e5f5d1591f87bb483dbd070b5b14a475
MasayoshiNomura <masayoshi.nomura@outlook.jp>
2018-10-11 13:37:38 +0900

    todau's update

--- MyFirsrApp/ViewController.swift
+++ MyFirsrApp/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var cameraView: UIView!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
